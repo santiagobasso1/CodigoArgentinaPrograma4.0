5d16c1c8c9d1f15d49340fd01e20f843d2f45f2f
Santiago <santiagobasso@hotmail.com>
2023-03-07 06:42:41 -0300

    Correcciónes Trabajos

--- clase3/clase3.js
+++ clase3/clase3.js
@@ -1,101 +1,86 @@
-// 1. Generar una función que le ingrese por parámetro algún dato ya sea ingresado por el
+// Generar una función que le ingrese por parámetro algún dato ya sea ingresado por el
 // usuario o declarado anteriormente en su código y mostrar por consola el tipo de dato
-// que es.
-//Arrow Function
-const tipoDeDato=(dato)=>{
-    return typeof(dato)
+// que es. 
+
+function tipoDeDatoDe(dato){
+    console.log(typeof(dato));
 }
 
-console.log("Ejercicio 1: ", tipoDeDato(23))
-// 2. Dado dos números ingresados por el usuario, se pide realizar una función que
+// Dado dos números ingresados por el usuario, se pide realizar una función que
 // devuelve la resta de ambos números. Mostrar el resultado por la consola.
-//Sin arrow function
-function resta (x,y){
+function resta(x,y){
     return x-y;
 }
-console.log("Ejercicio 2: ",resta(3,2))
+console.log(resta(5,1))
 
-// 3. Generar una función que le ingresen por parámetro dos valores distintos en dos
+// Generar una función que le ingresen por parámetro dos valores distintos en dos
 // variables a y b, luego el valor ingresado en a pasarlo a b y el valor Ingresado en b,
 // Pasarlo a a y mostrarlos.
 
-const pasaje=(a,b)=>{
-    let aux=a;
-    a=b;
-    b=aux;
-    console.log("Ejercicio 3: a: ",a,"b: ",b)
+function change(a,b){
+    console.log("Valor de a antes del cambio: ",a);
+    console.log("Valor de b antes del cambio: ",b);
+    let aux;
+    aux = a;
+    a = b;
+    b= aux;
+    console.log("Valor de a despues del cambio: ",a);
+    console.log("Valor de b despues del cambio: ",b);
 }
-pasaje(5,10);
-// 4. Generar una función que le ingrese por parámetro el valor del lado de un cuadrado
+change(1,2);
+
+// Generar una función que le ingrese por parámetro el valor del lado de un cuadrado
 // calcular su perímetro, su superficie, e informar los mismos en consola.
-const cuadrado =(lado) =>{
+
+function datosCuadrado(lado){
     let perimetro = lado*4;
-    let superficie = Math.pow(lado,2);
-    console.log("El perímetro es: ",perimetro,"Su área es: ",superficie);
+    let area = lado*lado;
+    console.log("El perímetro del cuadrado es: ",perimetro," El área del cuadrado es: ",area);
 }
-cuadrado(2)
+datosCuadrado(3);
 
-
-// 5. Generar una función que dada una temperatura en grados fahrenheit los convierta a
+// Generar una función que dada una temperatura en grados fahrenheit los convierta a
 // grados celsius.
-
-const fahrenheitToCelsius = (fahrenheit)=>{
-    return ((fahrenheit-32)*5)/9;
+function fahrToCel(gradosF){
+    return (gradosF - 32) *(5/9);
 }
-console.log(fahrenheitToCelsius(600))
+console.log(fahrToCel(300));
 
-// 6. Realizar una función que calcule el factorial de un número ingresado por el usuario (el
-// número no puede ser mayor de 10, realizar la validación). Mostrar el resultado por la
-// consola.
 
-const factorial = (x)=>{
-    let resultado = 1;
-    if (x<=10){
-        for (let i = 1; i<=x ;i++){
-            resultado = resultado*i;            
-        }
-        return resultado;
-    }else{
-        return "Debe ingresar un valor menor o igual a 10"
+// Realizar una función que calcule el factorial de un número ingresado por el usuario (el
+//     número no puede ser mayor de 10, realizar la validación). Mostrar el resultado por la
+//     consola.
+function factorialDe(n){
+    let resultado=1;
+    for (let i = 1; i<=n; i++){
+        resultado = resultado*i
     }
+    return resultado;
 }
-console.log(factorial(9));
-// 7. Dado una cadena de caracteres ingresada por el usuario, se pide realizar un script que
-// indique si ese texto es un palíndromo. Ejemplo de palíndromo: “Somos o no somos”.
+console.log(factorialDe(10));
 
-//Tiene que haber otra forma de saber si es palindroma o no
-const invertirCadena= (cadena)=>{
-    let cadenaMinuscula=cadena.toLowerCase()
-    let cadenaSinEspacios = cadenaMinuscula.replace(/ /g, "")
-    let arrayCadena = cadenaSinEspacios.split("");
-    let arrayInvertido = arrayCadena.reverse();
-    let stringResultante = arrayInvertido.join("");
-    if (cadenaSinEspacios === stringResultante){
-        return "La cadena es palintroma"
-    }else
-    {
-        return "La cadena NO ES PALINDROMA"
-    }
+// Dado una cadena de caracteres ingresada por el usuario, se pide realizar un script que
+// indique si ese texto es un palíndromo. Ejemplo de palíndromo: “Somos o no somos"
+function esPalindromo(cadena) 
+{
+    let cadenaSinEspacios = cadena.replace(/ /g,'');
+    let cadenaFormaArray=cadenaSinEspacios.split("");
+    let arrayInverso = cadenaFormaArray.reverse();
+    let cadenaInversa = arrayInverso.join("");
+    return cadenaSinEspacios==cadenaInversa;
 }
-console.log(invertirCadena("Somos o no somos"))
+console.log(esPalindromo("somos o no somos"))
 
-
-// 8. Dado una cadena de caracteres ingresada por el usuario, se pide realizar un script que
+// Dado una cadena de caracteres ingresada por el usuario, se pide realizar un script que
 // lo muestre en mayúsculas y luego en minúsculas. Investigue el manejo de string en
 // Javascript.
-
-
-const mayusMinuscula = (cadena)=>{
-    let minuscula = cadena.toLowerCase();
-    let mayuscula = cadena.toUpperCase();
-    return "Minuscula: "+minuscula+" Mayuscula: "+mayuscula
+function caracteresUpperLowerCase(cadena){
+    console.log("Cadena en minúsculas: ",cadena.toLowerCase());
+    console.log("Cadena en mayúsculas: ",cadena.toUpperCase());
 }
-console.log(mayusMinuscula("oajwd"))
+caracteresUpperLowerCase("prueba PRUEBA");
 
-
-
-
-// 9. Se pide que un usuario ingrese una nota numérica de un alumno. Muestra la
+// Se pide que un usuario ingrese una nota numérica de un alumno. Muestra la
 // calificación resultante según la nota ingresada:
 // 0-3: Muy deficiente
 // 3-5: Insuficiente
@@ -104,78 +89,77 @@ console.log(mayusMinuscula("oajwd"))
 // 7-9: Notable
 // 9-10: Sobresaliente
 
-
-const evaluar =(nota)=>{
-    if (nota>0 && nota <=3){
-        return "Muy Deficiente";
-    }else if (nota>3 && nota <=5){
-        return "Insuficiente"
-    }else if (nota>5 && nota <=6){
-        return "Suficiente"
-    }else if (nota>6 && nota <=7){
-        return "Bien"
-    }else if (nota>7 && nota <=9){
-        return "Notable"
-    }else if (nota>9 && nota <=10){
-        return "Sobresaliente"
-    }else{
-        return "Ingrese una opcion valida"
-    }
-}
-console.log(evaluar(9))
-
-// 10. Se pide realizar un script en el que el usuario introduce el número del mes (1 al 12) y
-// devuelve si ese mes tiene 30 o 31 días.
-
-const diasDelMes = (numeroMes) =>{
-    switch (numeroMes) {
-        case 1:
-        case 3:
-        case 5:
-        case 7:
-        case 8:
-        case 10:
-        case 12:
-            return "Este mes tiene 31 dias"                
-        case 4:
-        case 6:
-        case 9:
-        case 11:
-            return "Este mes tiene 30"   
-        case 2:
-            return "Febrero tiene 28 dias"                                                                                                                                          
+function calificar (nota){
+    switch (true) {
+        case nota>=0 && nota<3:
+            console.log("Muy deficiente")
+        break;
+        case nota>=3 && nota<5:
+            console.log("Insuficiente")
+        break;
+        case nota>=5 && nota<6:
+            console.log("Suficiente")
+        break;
+        case nota>=6 && nota<7:
+            console.log("Bien")
+        break;        
+        case nota>=7 && nota<9:
+            console.log("Notable")
+        break;   
+        case nota>=9 && nota<=10:
+            console.log("Sobresaliente")
+        break;                 
         default:
+            console.log(nota," No es una nota valida")
             break;
     }
 }
-
-console.log(diasDelMes(1))
-
+calificar(8.99999)
 
 
-// 11. Crear un script que genere una pirámide como el ejemplo con los números del 1 al
-// número que ingrese el usuario (no puede ser mayor de 10, realizar la validación):
-// 1
-// 12
-// 123
-// 1234
-// 12345
-// 123456
-
-
-const contarNumerosHasta = (n)=>{
-    for (let i = 1; i <= n; i++) {
-        console.log(i)        
+// 0. Se pide realizar un script en el que el usuario introduce el número del mes (1 al 12) y
+// devuelve si ese mes tiene 30 o 31 días.
+function diasDelMes(mes){
+    switch (true) {
+        case mes==1 || mes==3 || mes==5 || mes==7 || mes==8 || mes==10 || mes==12:
+            console.log("Este mes tiene 31 dias");
+        break;
+        case mes==4 || mes==6 || mes==9 || mes==11:
+            console.log("Este mes tiene 30");
+        break;
+        case mes == 2:
+            console.log("Este mes tiene 28 dias");
+        break;
+        default:
+            console.log(mes," No es un mes valido");
+        break;
     }
 }
-contarNumerosHasta(5);
+diasDelMes(4)
 
-// 12. Generar una función donde ingresen dos números, el primero corresponde a la
+// Crear un script que genere una pirámide como el ejemplo con los números del 1 al
+// número que ingrese el usuario (no puede ser mayor de 10, realizar la validación):
+function piramideNumerica (n)
+{
+    if (n<=10 && n>0){
+        let piramide=1;
+        for (let i = 2; i<=n; i++)
+        {
+            piramide=piramide+i.toString();
+            console.log(piramide);
+        }
+            
+    }else{
+        console.log("No puede ingresar un numero mayor a 10 ni menor a 0")
+    }
+}
+piramideNumerica(9)
+
+// Generar una función donde ingresen dos números, el primero corresponde a la
 // cantidad de bultos en stock y el segundo a la cantidad de bultos que se pueden
 // colocar en una caja. Calcule cuántas cajas completas se pueden llenar con los bultos
-// disponibles y cuantos bultos sueltos quedarían. Mostrar ambos resultados en consola.
-
-const contadorStock = (bultos,cantidadPorCaja)=>{
+// disponibles y cuantos bultos sueltos quedarían. Mostrar ambos resultados en consola
+function contadorStock (bultos,cantidadPorCaja){
     const cajasLlenas = bultos/cantidadPorCaja;
     const bultosSobrantes = bultos % cantidadPorCaja;
     console.log("Cantidad de cajas llenas: "+Math.trunc(cajasLlenas));
@@ -184,14 +168,11 @@ const contadorStock = (bultos,cantidadPorCaja)=>{
 
 contadorStock(65,9);
 
-
-// 13. Se pide realizar un script para una tienda de coches: Si el coche a la venta es un Ford
+// Se pide realizar un script para una tienda de coches: Si el coche a la venta es un Ford
 // Fiesta (código "fiesta"), el descuento es de un 5%. Si el coche a la venta es un Ford
 // Focus (código "focus"), el descuento es del 10%. El usuario introduce el artículo a
 // través de su código y el script saca el descuento correspondiente por la consola.
-
-
-const venderCoche = (nombre,precio) =>{
+function venderCoche (nombre,precio){
     if (nombre.toLowerCase()==="fiesta"){
         return precio-(precio*0.05)
     }else if (nombre.toLowerCase()==="focus"){
@@ -203,5 +184,4 @@ const venderCoche = (nombre,precio) =>{
 
 console.log(venderCoche("fiesta",3000))
 console.log(venderCoche("focus",3000))
-console.log(venderCoche("camaro",3000))
-
+console.log(venderCoche("twingo",3000))
